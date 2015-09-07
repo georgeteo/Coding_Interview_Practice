3898884bb4b2d8c43058a5b2eedd9950891afe9e
georgeteo <george.c.teo@gmail.com>
2015-09-07 16:49:05 -0700

    adding CCC 1.7, 1.8 question only

---
+++ chapter_1/1.7.py
@@ -0,0 +1,3 @@
+''' 
+CCC 1.7: Given an NXN matrix, if an element = 0, make that row and col 0
+'''
---
+++ chapter_1/1.8.py
@@ -0,0 +1,4 @@
+# CCC 1.8: Given a function isSubString(s1, s2), which determines if s1 is a 
+# substring of s2, write a function isRotation(s1, s2) that determines whehter 
+# s1 is a rotation of s2 calling isSubString only once. 
+# An example of a rotation is: apples and plesap. 
