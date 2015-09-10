122db46631cfdc3fd39212570b19ccdc8dafa70e
georgeteo <george.c.teo@gmail.com>
2015-09-09 20:17:55 -0700

    CCC 2.1

---
+++ chapter_2/2.1.py
@@ -0,0 +1,21 @@
+'''
+CCC 2.1:
+1. Remove duplicates from an unsorted linked list.
+We assume doubly lined list, but honestly does not matter.
+2. Do it without temporary variables
+'''
+
+import linked_list
+
+def remove_duplicate_with_temp_var(linked_list):
+    '''
+    O(n)
+    '''
+    duplicates = dict()
+    node = linked_list.first
+    while node:
+        try dict[node.data]:
+            linked_list.remove(node)
+        except KeyError:
+            dict[node.data] = 1
+        node = node.next
