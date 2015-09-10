92fa064e4bbffbf1af49caeb74ccb33ba1b000f2
georgeteo <george.c.teo@gmail.com>
2015-09-09 20:46:01 -0700

    CCC 2.3

---
+++ chapter_2/2.3.py
@@ -0,0 +1,5 @@
+'''
+CCC 2.3: Implement an algorithm to delete a node in the middle of a singly linked list given only access to that node
+
+Solution: copy contents of next node and update the pointer to jump that node.
+'''
